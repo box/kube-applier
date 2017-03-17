e108aae44f2e169b7f860ca262f75a9f76cd79c2
Hakan Baba <hakanbaba83@gmail.com>
2017-03-16 18:41:28 -0700

    Simple unit test executer in a docker container

--- .rosie/rosie-unit-tests.sh
+++ .rosie/rosie-unit-tests.sh
@@ -4,15 +4,11 @@
 # Volume mounts the source of the repo. Sets up the GOPATH and runs go test
 # in the project root
 
+set -x
+
 cwd=$(pwd)
-godir=/go/src/github.com/box
+gopath=/go
+repodir=${gopath}/src/github.com/box/kube-applier
 
-sudo docker run -it docker-registry-vip.dev.box.net/jenkins/box-sl6 \
-    -v ${cwd}:${godir} bash -c \"ls\"
-
-
-
-
-
-
+sudo docker run -v ${cwd}:${repodir} docker-registry-vip.dev.box.net/jenkins/box-sl6-build-golang bash -c "export GOPATH=${gopath}; cd ${repodir}; go test -v ./... "
 
