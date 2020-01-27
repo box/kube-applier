06225e2d78de2ae8827bfeb41f275cd348d813c1
Rob Best <robertbest89@gmail.com>
2020-01-27 15:47:52 +0000

    log at debug level when waiting for the src dir
    I've observed that it typically takes about 1m for the src dir to become available when
    using git-sync to clone our kubernetes manifests. With the log messages as they are now,
    this results in ~60 messages logged at WARN level before the directory is found.
    
    This seems wrong and could potentially be misleading, given that this is essentially the
    application behaving as expected. So,  I've changed the log level of that message to DEBUG
    and also added a couple of INFO level messages to communicate what is happening.

--- sysutil/filesystem.go
+++ sysutil/filesystem.go
@@ -28,6 +28,7 @@ func ListDirs(rootPath string) ([]string, error) {
 
 // WaitForDir returns when the specified directory is located in the filesystem, or if there is an error opening the directory once it is found.
 func WaitForDir(path string, interval, timeout time.Duration) error {
+	log.Logger.Info("Waiting for the source directory", "path", path, "timeout", timeout.String())
 
 	to := time.After(timeout)
 
@@ -52,13 +53,14 @@ func WaitForDir(path string, interval, timeout time.Duration) error {
 						err,
 					)
 				}
-				log.Logger.Warn("Failed to get dir info", err)
+				log.Logger.Debug("Failed to get dir info", "path", path, "error", err)
 			} else if !f.IsDir() {
 				return fmt.Errorf(
 					"Error: %v is not a directory",
 					path,
 				)
 			} else {
+				log.Logger.Info("Found the source directory", "path", path)
 				return nil
 			}
 		}
