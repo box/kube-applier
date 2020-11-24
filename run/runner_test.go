53fc1bfcf6fc84a082bc6288f7fc9555419cff11
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-11-24 15:05:30 +0000

    Add tests for strongbox integration
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- run/runner_test.go
+++ run/runner_test.go
@@ -1,6 +1,7 @@
 package run
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 	"strings"
@@ -12,7 +13,10 @@ import (
 	. "github.com/onsi/gomega/gstruct"
 	gomegatypes "github.com/onsi/gomega/types"
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kubeapplierv1alpha1 "github.com/utilitywarehouse/kube-applier/apis/kubeapplier/v1alpha1"
 	"github.com/utilitywarehouse/kube-applier/git"
@@ -244,17 +248,188 @@ Error from server (NotFound): error when creating "../testdata/manifests/app-c/d
 			}
 		})
 	})
+
+	Context("When operating on an Application that defines a strongbox keyring", func() {
+		It("Should be able to apply encrypted files, given a strongbox keyring secret", func() {
+			ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "app-d"}}
+			Expect(testKubeClient.Create(context.TODO(), ns)).To(BeNil())
+
+			secret := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "strongbox",
+					Namespace: ns.Name,
+				},
+				StringData: map[string]string{
+					".strongbox_keyring": `keyentries:
+- description: foobar
+  key-id: G4M/cCqr+LZtEyQbAjSu5SMEcnVTj2IkWahrkOUq/J4=
+  key: QxK6PHX37IybXRshJZy4IXRjCdFFsE0wdiYlfeGP1QA=`,
+				},
+				Type: corev1.SecretTypeOpaque,
+			}
+			Expect(testKubeClient.Create(context.TODO(), secret)).To(BeNil())
+			secretEmpty := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "strongbox-empty",
+					Namespace: ns.Name,
+				},
+				Type: corev1.SecretTypeOpaque,
+			}
+			Expect(testKubeClient.Create(context.TODO(), secretEmpty)).To(BeNil())
+
+			appList := []kubeapplierv1alpha1.Application{
+				{
+					TypeMeta: metav1.TypeMeta{APIVersion: "kube-applier.io/v1alpha1", Kind: "Application"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "appD",
+						Namespace: ns.Name,
+					},
+					Spec: kubeapplierv1alpha1.ApplicationSpec{
+						Prune:          true,
+						RepositoryPath: "app-d",
+					},
+				},
+				{
+					TypeMeta: metav1.TypeMeta{APIVersion: "kube-applier.io/v1alpha1", Kind: "Application"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "appD",
+						Namespace: ns.Name,
+					},
+					Spec: kubeapplierv1alpha1.ApplicationSpec{
+						Prune:                     true,
+						RepositoryPath:            "app-d",
+						StrongboxKeyringSecretRef: "invalid",
+					},
+				},
+				{
+					TypeMeta: metav1.TypeMeta{APIVersion: "kube-applier.io/v1alpha1", Kind: "Application"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "appD",
+						Namespace: ns.Name,
+					},
+					Spec: kubeapplierv1alpha1.ApplicationSpec{
+						Prune:                     true,
+						RepositoryPath:            "app-d",
+						StrongboxKeyringSecretRef: secretEmpty.Name,
+					},
+				},
+				{
+					TypeMeta: metav1.TypeMeta{APIVersion: "kube-applier.io/v1alpha1", Kind: "Application"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "appD",
+						Namespace: ns.Name,
+					},
+					Spec: kubeapplierv1alpha1.ApplicationSpec{
+						Prune:                     true,
+						RepositoryPath:            "app-d",
+						StrongboxKeyringSecretRef: secret.Name,
+					},
+				},
+			}
+
+			headCommitHash, err := (&git.Util{RepoPath: testRunner.RepoPath}).HeadHashForPaths("app-d")
+			Expect(err).To(BeNil())
+			Expect(headCommitHash).ToNot(BeEmpty())
+
+			expectedStatus := []*kubeapplierv1alpha1.ApplicationStatusRun{
+				{
+					Command:      "",
+					Commit:       headCommitHash,
+					ErrorMessage: "exit status 1",
+					Finished:     metav1.Time{},
+					Output: `namespace/app-d configured
+error: error validating "../testdata/manifests/app-d/deployment.yaml": error validating data: invalid object to validate; if you choose to ignore these errors, turn validation off with --validate=false
+`,
+					Started: metav1.Time{},
+					Success: false,
+					Type:    PollingRun.String(),
+				},
+				{
+					Command:      "^.*$",
+					Commit:       "",
+					ErrorMessage: `secrets "invalid" not found`,
+					Finished:     metav1.Time{},
+					Output:       "",
+					Started:      metav1.Time{},
+					Success:      false,
+					Type:         PollingRun.String(),
+				},
+				{
+					Command:      "^.*$",
+					Commit:       "",
+					ErrorMessage: `Secret app-d/strongbox-empty does not contain key '.strongbox_keyring'`,
+					Finished:     metav1.Time{},
+					Output:       "",
+					Started:      metav1.Time{},
+					Success:      false,
+					Type:         PollingRun.String(),
+				},
+				{
+					Command:      "",
+					Commit:       headCommitHash,
+					ErrorMessage: "",
+					Finished:     metav1.Time{},
+					Output: `namespace/app-d unchanged
+deployment.apps/test-deployment created
+`,
+					Started: metav1.Time{},
+					Success: true,
+					Type:    PollingRun.String(),
+				},
+			}
+
+			// construct expected app list
+			expected := make([]kubeapplierv1alpha1.Application, len(appList))
+			for i := range appList {
+				expected[i] = appList[i]
+				expected[i].Status = kubeapplierv1alpha1.ApplicationStatus{LastRun: expectedStatus[i]}
+			}
+
+			for i := range appList {
+				testRunQueue <- Request{
+					Type:        PollingRun,
+					Application: &appList[i],
+				}
+			}
+
+			Eventually(
+				func() bool {
+					deployment := &appsv1.Deployment{}
+					return testKubeClient.Get(context.TODO(), client.ObjectKey{Namespace: "app-d", Name: "test-deployment"}, deployment) == nil
+				},
+				time.Second*15,
+				time.Second,
+			).Should(BeTrue())
+
+			testRunner.Stop()
+
+			for i := range appList {
+				Expect(appList[i]).Should(matchApplication(expected[i], testKubectlPath, testRunner.RepoPath, testApplyOptions.PruneWhitelist(&appList[i], testRunner.PruneBlacklist)))
+			}
+		})
+	})
 })
 
 func matchApplication(expected kubeapplierv1alpha1.Application, kubectlPath, repoPath string, pruneWhitelist []string) gomegatypes.GomegaMatcher {
-	commandExtraArgs := expected.Status.LastRun.Command
-	if expected.Spec.DryRun {
-		commandExtraArgs += " --dry-run=server"
-	} else {
-		commandExtraArgs += " --dry-run=none"
-	}
-	if expected.Spec.Prune {
-		commandExtraArgs += fmt.Sprintf(" --prune --all --prune-whitelist=%s", strings.Join(pruneWhitelist, " --prune-whitelist="))
+	commandMatcher := Ignore()
+	if expected.Status.LastRun.Command != "^.*$" {
+		commandExtraArgs := expected.Status.LastRun.Command
+		if expected.Spec.DryRun {
+			commandExtraArgs += " --dry-run=server"
+		} else {
+			commandExtraArgs += " --dry-run=none"
+		}
+		if expected.Spec.Prune {
+			commandExtraArgs += fmt.Sprintf(" --prune --all --prune-whitelist=%s", strings.Join(pruneWhitelist, " --prune-whitelist="))
+		}
+		commandMatcher = MatchRegexp(
+			"^%s --server %s apply -f [^ ]+/%s -R -n %s%s",
+			kubectlPath,
+			testConfig.Host,
+			expected.Spec.RepositoryPath,
+			expected.Namespace,
+			commandExtraArgs,
+		)
 	}
 	return MatchAllFields(Fields{
 		"TypeMeta":   Equal(expected.TypeMeta),
@@ -262,14 +437,7 @@ func matchApplication(expected kubeapplierv1alpha1.Application, kubectlPath, rep
 		"Spec":       Equal(expected.Spec),
 		"Status": MatchAllFields(Fields{
 			"LastRun": PointTo(MatchAllFields(Fields{
-				"Command": MatchRegexp(
-					"^%s --server %s apply -f [^ ]+/%s -R -n %s%s",
-					kubectlPath,
-					testConfig.Host,
-					expected.Spec.RepositoryPath,
-					expected.Namespace,
-					commandExtraArgs,
-				),
+				"Command":      commandMatcher,
 				"Commit":       Equal(expected.Status.LastRun.Commit),
 				"ErrorMessage": Equal(expected.Status.LastRun.ErrorMessage),
 				"Finished": And(
